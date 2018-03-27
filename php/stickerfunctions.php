0748eb1e9946488643c9205a5919ab31a2386757
Milo Rupp <badtzmaru139@gmail.com>
2018-03-27 13:13:36 -0700

    alright the reset works

--- php/stickerfunctions.php
+++ php/stickerfunctions.php
@@ -1,10 +1,10 @@
 <?php
 
 require_once("connection.php");
-require_once("../../attendance_system/connection.php");
+require_once("attendanceconnection.php");
 
-function resetstickers(){
-	global $db_attendance;
+function resetStickers(){
+	global $db_server;
 	global $db_stickers;
 	$db_stickers->query("TRUNCATE `offerings`");
 	$rss = new DOMDocument();
@@ -29,27 +29,61 @@ function resetstickers(){
 				$src = $matches[1];
 				$k['content'] = $src;
 		}
-		
 	}
 	
-	$zero = "0";
-	
 	foreach ($feed as $class) {
-	
 		$facilitator = get_teacher($class['link']);
-	
-		$block = is_block($class['link']);
-	
-		$stmt = $db_stickers->prepare("INSERT INTO offerings (classname,facilitator,category,description,link,image,block,blackstickers,greystickers,whitestickers) VALUES (?,?,?,?,?,?,?,?,?,?)");
-	
-		$stmt->bind_param('ssssssssss', $class['title'], $facilitator, $class['category'], $class['desc'],$class['link'], $class['content'], $block, $zero, $zero, $zero);
-	
+        $block = is_block($class['link']);
+        
+		$stmt = $db_stickers->prepare("INSERT INTO offerings (`class-name`,`facilitator`,`category`,`description`,`link`,`image`,`block`) VALUES (?,?,?,?,?,?,?)");
+		$stmt->bind_param('sssssss', $class['title'], $facilitator, $class['category'], $class['desc'],$class['link'], $class['content'], $block);
 		$stmt->execute();
-	
 		$stmt->close();
     }
-	
 
+	$db_stickers->query("truncate stickers");
+    $getallottedstickers = $db_stickers->query("SELECT * FROM allotted");
+    $allottedstickers = $getallottedstickers->fetch_row();
+    $getstudents = $db_server->query("SELECT * FROM studentdata WHERE current=1");
+
+    $studentinfo = array();
+	while ($student_data = $getstudents->fetch_assoc()) {
+		array_push($studentinfo, $student_data);
+	}
+	
+    foreach($studentinfo as $student){
+        $stmt = $db_stickers->prepare("INSERT INTO stickers (`student-id`,`black`,`grey`,`white`,`blockblack`,`blockgrey`,`blockwhite`) VALUES (?,?,?,?,?,?,?)");
+        $stmt->bind_param('iiiiiii', $student['studentid'], $allottedstickers[0], $allottedstickers[1], $allottedstickers[2],$allottedstickers[3],$allottedstickers[4],$allottedstickers[5]);
+        $stmt->execute();
+        $stmt->close();
+
+    }
+
+echo "Reset complete!";
+
+}
+
+function get_teacher($url){
+	$text = file_get_contents($url);
+	$regex = "/<span>(.*?)<\/span>/";
+	
+	if (preg_match_all($regex, $text ,$matches)) {
+		$string = $matches[1][0];
+		$string = str_replace("(", null, $string);
+		$string = str_replace(")", null, $string);
+		return $string;
+	}
+}
+
+function is_block($url){
+	$text = file_get_contents($url);
+	$regex = "/<span class=block>(.*?)<\/span>/";
+	
+	if (strpos($text, '<span class="block">This is a block class.</span>')) {
+		return TRUE;
+	} else {
+		return FALSE;
+	}
 }
 
 ?>
---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once("php/stickerfunctions.php");
+
+resetStickers();
+
+?>
